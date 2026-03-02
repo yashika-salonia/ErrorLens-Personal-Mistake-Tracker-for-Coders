d656a67a0e4f5c2c5854247b8202886057ad83ed
triptibhatnagar <bhatnagartripti2727@gmail.com>
2026-03-02 09:40:36 +0530

    using middleware in server.js

--- server/server.js
+++ server/server.js
@@ -2,10 +2,16 @@ require('dotenv').config()
 const express = require('express')
 const connectDB = require('./config/db')
 const cors = require('cors')
+const errorHandler = require('./middleware/errorMiddleware')
+const helmet = require("helmet")
+
 
 const app = express()
-app.use(cors())
+
 app.use(express.json())
+app.use(cors())
+app.use(helmet())
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
